bec52eac10dc05f75ea03d5c4338076a9475cf18
Erik Johansson <erij@kth.se>
2021-12-04 22:38:15 +0100

    Added location and timetime for holy grail item findings

--- routes/holy_grail_findings.js
+++ routes/holy_grail_findings.js
@@ -16,11 +16,12 @@ router.get('/', function (req, res, next) {
 router.post('/', function (req, res, next) {
     try {
         console.log(req.fields);
-        const { player_name, player_password, holy_grail_item_name } = req.fields;
+        const { player_name, player_password, holy_grail_item_name, holy_grail_item_location } = req.fields;
         const player_id = db.instance.prepare(`SELECT player_id FROM player WHERE lower(player_name) = lower(?) AND player_password = ?`).get([player_name, player_password]);
         const holy_grail_item_id = db.instance.prepare(`SELECT holy_grail_item_id FROM holy_grail_item WHERE item_name = ?`).get(holy_grail_item_name);
-        console.log("Inserting new holy grail item finding for player_id:" + JSON.stringify(player_id) + ", item id:" + JSON.stringify(holy_grail_item_id));
-        const result = db.instance.prepare(`INSERT INTO holy_grail_item_finding (player_id, holy_grail_item_id, finding_date, finding_location) VALUES (?, ?, '2020-01-01 10:00:00', '')`).run(player_id.player_id, holy_grail_item_id.holy_grail_item_id);
+        console.log("Inserting new holy grail item finding for player_id:" + JSON.stringify(player_id) + ", item id:" + JSON.stringify(holy_grail_item_id) + ", location:" + JSON.stringify(holy_grail_item_location));
+
+        const result = db.instance.prepare(`INSERT INTO holy_grail_item_finding (player_id, holy_grail_item_id, finding_date, finding_location) VALUES (?, ?, datetime('now'), ?)`).run(player_id.player_id, holy_grail_item_id.holy_grail_item_id, holy_grail_item_location);
         if (result.changes) {
             const rune_data = db.instance.prepare(`SELECT rune_name, date(finding_date) FROM finding JOIN player ON finding.player_id = player.player_id JOIN rune ON finding.rune_id = rune.rune_id WHERE finding.player_id = ?`).all([player_id.player_id]);
             const holy_grail_item_data = db.instance.prepare(`SELECT item_name, date(finding_date) FROM holy_grail_item_finding JOIN player ON holy_grail_item_finding.player_id = player.player_id JOIN holy_grail_item ON holy_grail_item_finding.holy_grail_item_id = holy_grail_item.holy_grail_item_id WHERE holy_grail_item_finding.player_id = ?`).all([player_id.player_id]);
