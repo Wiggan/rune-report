13e5309c5fee8ac4158a8fb568007cd93e84ea9b
Erik Johansson <erij@kth.se>
2021-12-11 13:06:14 +0100

    Added a rune listings

--- routes/stats.js
+++ routes/stats.js
@@ -4,14 +4,13 @@ const db = require('../services/db');
 
 router.get('/', function (req, res, next) {
     try {
-        const player_name = 'Erik';
-        const rune_data = db.instance.prepare(`SELECT rune_name, date(finding_date) FROM finding JOIN player ON finding.player_id = player.player_id JOIN rune ON finding.rune_id = rune.rune_id WHERE finding.player_id = ?`).all([player_name]);
+        const all_runes = db.instance.prepare(`SELECT player_name, rune_name, finding_date as 'finding_date' FROM finding JOIN rune ON rune.rune_id = finding.rune_id JOIN player ON player.player_id = finding.player_id ORDER BY date(finding_date) ASC`).all([]);
         res.render('stats',
             // { player_name: player_name, runes: rune_data, holy_grail_items: holy_grail_item_data }
-            { player_name: 'Test', runes: rune_data }
+            { all_runes: all_runes }
         );
     } catch (err) {
-        console.error(`Error while getting runes `, err.message);
+        console.error(`Error while getting runes`, err.message);
         next(err);
     }
 });
