2ca374eaf576ce115f9239d8cc75de0e63979bb6
Erik Johansson <erij@kth.se>
2022-11-30 21:34:18 +0100

    new season

--- sql/insert_new_season.sql
+++ sql/insert_new_season.sql
@@ -15,7 +15,8 @@ BEGIN TRANSACTION;
 
 --CREATE TABLE season (season_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, start_date DATETIME DEFAULT (CURRENT_TIMESTAMP) NOT NULL);
 
-INSERT INTO season(season_id, start_date) VALUES (5, '2022-06-24 19:00:00');
+INSERT INTO season(season_id, start_date) VALUES (6, '2022-06-25 19:00:00');
+--INSERT INTO season(season_id, start_date) VALUES (5, '2022-06-24 19:00:00');
 --INSERT INTO season(season_id, start_date) VALUES (4, '2021-12-03 19:00:00');
 
 COMMIT TRANSACTION;
